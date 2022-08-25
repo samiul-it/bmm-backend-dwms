9270d8ff47b5d8d342683d9eb801668b155283e8
rushikesh.patil <rushikesh.patil@infraplan.co.in>
2022-08-25 12:26:41 +0530

    socket portchanged

--- src/notification/notification.gateway.ts
+++ src/notification/notification.gateway.ts
@@ -8,7 +8,7 @@ import {
 import { NotificationService } from './notification.service';
 import { WsGuard } from '../WSGuard';
 
-@WebSocketGateway(Number(process.env.PORT) || 5001, { cors: '*' })
+@WebSocketGateway(5001, { cors: '*' })
 export class NotificationGateway {
   constructor(private notificationService: NotificationService) {}
 
